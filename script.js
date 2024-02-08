e45feaa75b1e7f0ea70816cfd4436fe567c2facd
fred <fred>
2024-02-08 12:49:13 -0800

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/*var menuIcon = document.querySelector(".menu-icon");
+var menuIcon = document.querySelector(".sidebar");
+
+menuIcon.onclick = function(){
+    sidebar .classlist .toggle("small-sidebar");
+    
+}
